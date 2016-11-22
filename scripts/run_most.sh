f372be1b9464e714c6f4a081d45a9c8b0bac8605
andrewjpage <andrewjpage@gmail.com>
2016-11-22 09:53:31 +0000

    run most

---
+++ scripts/run_most.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# Given a directory containing FASTQ files, and an output directory, run using Docker.
+# run_most.sh /mnt/docker/data/coverage /data/MOST 3d09478f7825
+
+if [ $# -ne 3 ]
+then
+    echo "Usage: `basename $0` host_base output_directory docker_hash"
+    exit 1
+fi
+
+HOST_BASE=$1
+OUTPUT_DIRECTORY=$2
+DOCKER_HASH=$3
+
+INPUT_DIRECTORY=/data
+
+mkdir -p $OUTPUT_DIRECTORY
+for FORWARD_FILE in $(find ${HOST_BASE} -type f -name "*_1.fastq.gz");
+  do
+    FORWARD_FILE=${FORWARD_FILE/${HOST_BASE}/${INPUT_DIRECTORY}}
+    REVERSE_FILE=${FORWARD_FILE/_1.fastq.gz/_2.fastq.gz}
+    BASE_NAME=${FORWARD_FILE/_1.fastq.gz/results}
+    BASE_NAME=${BASE_NAME##*/}
+    docker run --rm -v ${HOST_BASE}:/data ${DOCKER_HASH} MOST.py -1 ${FORWARD_FILE} -2 ${REVERSE_FILE} -o ${OUTPUT_DIRECTORY}/output_${BASE_NAME} -st /MOST/MLST_data/salmonella
+done
