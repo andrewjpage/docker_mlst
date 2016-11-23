a460970ab50a4cf2b0b786bf6f3d23da218bd3c9
andrewjpage <andrewjpage@gmail.com>
2016-11-23 13:21:08 +0000

    assemble with spades phred

--- scripts/assemble_with_spades.sh
+++ scripts/assemble_with_spades.sh
@@ -1,21 +1,25 @@
 #! /bin/bash
 # Given a directory of FASTQ files and an output directory, produce assemblies with spades
-# run_most.sh /mnt/docker/data/coverage/fastqs /mnt/docker/data/coverage/fastas
+# run_most.sh /mnt/docker/data/coverage /data /data/fastas
 
-if [ $# -ne 2 ]
+if [ $# -ne 3 ]
 then
-    echo "Usage: `basename $0` input_directory output_directory"
+    echo "Usage: `basename $0` host_base input_directory output_directory"
     exit 1
 fi
 
-INPUT_DIRECTORY=$1
-OUTPUT_DIRECTORY=$2
+HOST_BASE=$1
+INPUT_DIRECTORY=$2
+OUTPUT_DIRECTORY=$3
+DOCKER_HASH=e8398b3e5a6a
 
-for FORWARD_FILE in $(find ${INPUT_DIRECTORY} -type f -name "*_1.fastq.gz");
+mkdir -p $OUTPUT_DIRECTORY
+for FORWARD_FILE in $(find ${HOST_BASE} -type f -name "*_1.fastq.gz");
   do
+    FORWARD_FILE=${FORWARD_FILE/${HOST_BASE}/${INPUT_DIRECTORY}}
     REVERSE_FILE=${FORWARD_FILE/_1.fastq.gz/_2.fastq.gz}
     BASE_NAME=${FORWARD_FILE/_1.fastq.gz/}
     BASE_NAME=${BASE_NAME##*/}
     
-    spades.py -t 1 -1 ${FORWARD_FILE} -2 ${REVERSE_FILE} -o ${OUTPUT_DIRECTORY}/${BASE_NAME}
+    { time docker run --rm -v ${HOST_BASE}:/data ${DOCKER_HASH} spades.py -t 1 -1 ${FORWARD_FILE} -2 ${REVERSE_FILE} -o ${OUTPUT_DIRECTORY}/${BASE_NAME} --phred-offset 33  > ${OUTPUT_DIRECTORY}/output_${BASE_NAME} ; }  2> ${HOST_BASE}/fastas/timings_${BASE_NAME}
 done
