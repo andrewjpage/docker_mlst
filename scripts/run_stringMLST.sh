41d9c1d6a43585e7e442667cdc267f434c4e2519
andrewjpage <andrewjpage@gmail.com>
2016-12-16 10:32:21 +0000

    lowercase stringmlst for docker image names

--- scripts/run_stringMLST.sh
+++ scripts/run_stringMLST.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # Given a directory containing FASTQ files, and an output directory, run using Docker.
-# run_stringMLST.sh /mnt/data/coverage /data/stringMLST stringMLST
+# run_stringMLST.sh /mnt/data/coverage /data/stringMLST stringmlst
 
 if [ $# -ne 3 ]
 then
