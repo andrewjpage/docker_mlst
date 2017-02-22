fdb673281c5b4bfaad9c950f0e7b4bebb94d8856
Andrew J. Page <andrewjpage@gmail.com>
2017-02-22 12:46:44 +0000

    Update mixed samples image

--- results/mixed/mixed_st_plots.R
+++ results/mixed/mixed_st_plots.R
@@ -2,6 +2,6 @@ library(ggplot2)
 all_data = read.table('mixed_st_calls.tsv',header = TRUE, sep = "\t")
 
 all_data$Mix = all_data$Mix*2
-ggplot(data = all_data, aes(y = Mix, x= Software,fill = ST))+  geom_tile( ) + theme_classic() + theme_bw(base_size = 16)  + xlab("Software")+ ylim(c(1,100))+  ylab("Percentage of S. Weltevreden")+ coord_flip()
+ggplot(data = all_data, aes(y = Mix, x= Software,fill = 'Predicted ST'))+  geom_tile( ) + theme_classic() + theme_bw(base_size = 16)  + xlab("Software")+ ylim(c(1,100))+  ylab("ST 365 (S.Weltevreden)")+ coord_flip()
 ggsave(filename="mixing_samples.png", scale=1)
 
